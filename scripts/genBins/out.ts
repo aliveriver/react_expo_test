815e1ba8960917d806d0c117afbf609e8313ec37
查无此人 <woshi123qaq@163.com>
2025-06-10 16:39:19 +0800

    模型 v0.1

---
+++ scripts/genBins/out.ts
@@ -0,0 +1,30 @@
+
+export const MODEL_JSON = require("../../assets/model/model.json");
+export const BIN_FILES = [
+    require("../../assets/model/group1-shard1of25.bin"),
+  require("../../assets/model/group1-shard2of25.bin"),
+  require("../../assets/model/group1-shard3of25.bin"),
+  require("../../assets/model/group1-shard4of25.bin"),
+  require("../../assets/model/group1-shard5of25.bin"),
+  require("../../assets/model/group1-shard6of25.bin"),
+  require("../../assets/model/group1-shard7of25.bin"),
+  require("../../assets/model/group1-shard8of25.bin"),
+  require("../../assets/model/group1-shard9of25.bin"),
+  require("../../assets/model/group1-shard10of25.bin"),
+  require("../../assets/model/group1-shard11of25.bin"),
+  require("../../assets/model/group1-shard12of25.bin"),
+  require("../../assets/model/group1-shard13of25.bin"),
+  require("../../assets/model/group1-shard14of25.bin"),
+  require("../../assets/model/group1-shard15of25.bin"),
+  require("../../assets/model/group1-shard16of25.bin"),
+  require("../../assets/model/group1-shard17of25.bin"),
+  require("../../assets/model/group1-shard18of25.bin"),
+  require("../../assets/model/group1-shard19of25.bin"),
+  require("../../assets/model/group1-shard20of25.bin"),
+  require("../../assets/model/group1-shard21of25.bin"),
+  require("../../assets/model/group1-shard22of25.bin"),
+  require("../../assets/model/group1-shard23of25.bin"),
+  require("../../assets/model/group1-shard24of25.bin"),
+  require("../../assets/model/group1-shard25of25.bin")
+  ] as number[];
+
